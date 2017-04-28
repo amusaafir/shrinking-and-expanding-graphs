75a9fd84b7bb8e9ffbfc1ed9f5bd5a726baf01fb
Ahmed <ahmedmusaafir@gmail.com>
2017-04-28 17:38:43 +0200

    First results expanding.

--- CompSysDemo/main.py
+++ CompSysDemo/main.py
@@ -11,13 +11,21 @@ facebook_graph = graph_loader.create_undirected_graph_from_file(graph_src_path)
 print 'Amount of nodes original (input) graph: ', facebook_graph.number_of_nodes()
 print 'Amount of edges original (input) graph: ', facebook_graph.number_of_edges()
 
-"""ties = TIES()
+graph_writer = GraphWriter()
+expander = Expander()
+facebook_expanded_graph = expander.expand(facebook_graph, 3)
+graph_writer.save_edge_list_graph_to_csv(facebook_expanded_graph, "expanded_fb_graph")
+
+"""
+# Ties test for Gephi #
+ties = TIES()
 sampled_graph = ties.sample(facebook_graph, 0.1)
 
 graph_writer = GraphWriter()
 graph_writer.save_edge_list_graph_to_csv(sampled_graph, "sampled_graph")
 """
 
+"""
 # Expanding test for Gephi #
 graph = nx.Graph()
 graph.add_edge(0, 1)
@@ -26,6 +34,4 @@ graph.add_edge(1, 2)
 
 expander = Expander()
 expanded_graph = expander.expand(graph, 3)
-
-graph_writer = GraphWriter()
-graph_writer.save_edge_list_graph_to_csv(expanded_graph, "expanded_graph")
+"""
