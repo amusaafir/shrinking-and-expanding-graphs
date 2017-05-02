d68e67ac967bb5acfebfe3fda7527f559fb7c1ac
Ahmed <ahmedmusaafir@gmail.com>
2017-05-03 01:41:44 +0200

    Added astro graph dataset with (expanding and sampling) output.

--- CompSysDemo/main.py
+++ CompSysDemo/main.py
@@ -2,37 +2,25 @@ from io.graph_loader import *
 from io.graph_writer import *
 from expander import *
 from graph_metrics import *
-import networkx as nx
 
 graph_metrics = GraphMetrics()
 facebook_graph_src = "datasets/facebook_graph.txt"
-email_enron_graph_src = "datasets/Email-Enron.csv"
+astro_graph_src = "datasets/CA-AstroPh_input.txt"
 graph_loader = GraphLoader()
-input_graph = graph_loader.create_undirected_graph_from_file(facebook_graph_src)
+input_graph = graph_loader.create_undirected_graph_from_file(astro_graph_src)
 print 'Amount of nodes original (input) graph: ', input_graph.number_of_nodes()
 print 'Amount of edges original (input) graph: ', input_graph.number_of_edges()
 
 graph_writer = GraphWriter()
 expander = Expander()
-facebook_expanded_graph = expander.expand(input_graph, 3)
-graph_writer.save_edge_list_graph_to_csv(facebook_expanded_graph, "expanded_fb_graph_new")
+expanded_graph = expander.expand(input_graph, 3)
+graph_writer.save_edge_list_graph_to_csv(expanded_graph, "expanded_astro_graph")
 
 """
 # Ties test for Gephi #
 ties = TIES()
-sampled_graph = ties.sample(input_graph, 1)
+sampled_graph = ties.sample(input_graph, 0.6)
 
 graph_writer = GraphWriter()
-graph_writer.save_edge_list_graph_to_csv(sampled_graph, "sampled_single_fb_graph_new")
-"""
-
-"""
-# Expanding test for Gephi #
-graph = nx.Graph()
-graph.add_edge(0, 1)
-graph.add_edge(0, 2)
-graph.add_edge(1, 2)
-
-expander = Expander()
-expanded_graph = expander.expand(graph, 3)
+graph_writer.save_edge_list_graph_to_csv(sampled_graph, "sampled_single_astro_graph")
 """
