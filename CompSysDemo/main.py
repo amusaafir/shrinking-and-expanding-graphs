c039cb79de8670211e4687d8ce51b9874e8188f4
Ahmed <amusaafir@users.noreply.github.com>
2017-05-11 00:41:03 +0200

    Refactored main

--- CompSysDemo/main.py
+++ CompSysDemo/main.py
@@ -1,29 +1,37 @@
 from algorithm.expanding.expander import *
-from algorithm.helper.graph_metrics import *
 from io.graph_loader import *
 from io.graph_writer import *
 from algorithm.expanding.topology.star import *
 from algorithm.expanding.topology.line import *
 
-graph_metrics = GraphMetrics()
+class Main():
+    def __init__(self):
+        self.graph_writer = GraphWriter()
+
+    def run_sampling(self, input_graph_src, output_file_name, fraction):
+        input_graph = self.create_undirected_graph_from_file(input_graph_src)
+        ties = TIES()
+        sampled_graph = ties.sample(input_graph, fraction)
+        self.graph_writer.save_edge_list_graph_to_csv(sampled_graph, output_file_name)
+
+    def run_expanding(self, input_graph_src, output_file_name):
+        input_graph = self.create_undirected_graph_from_file(input_graph_src)
+        expander = Expander()
+        expanded_graph = expander.expand(input_graph, 3, LineTopology())
+        self.graph_writer.save_edge_list_graph_to_csv(expanded_graph, output_file_name)
+
+    def create_undirected_graph_from_file(self, input_file):
+        graph_loader = GraphLoader()
+        input_graph = graph_loader.create_undirected_graph_from_file(input_file)
+        print 'Amount of nodes original (input) graph: ', input_graph.number_of_nodes()
+        print 'Amount of edges original (input) graph: ', input_graph.number_of_edges()
+
+        return input_graph
+
+main = Main()
 facebook_graph_src = "datasets/facebook_graph.txt"
 astro_graph_src = "datasets/CA-AstroPh_input.txt"
 ca_hepph_src = "datasets/CA-HepPh.txt"
-graph_loader = GraphLoader()
-input_graph = graph_loader.create_undirected_graph_from_file(facebook_graph_src)
-print 'Amount of nodes original (input) graph: ', input_graph.number_of_nodes()
-print 'Amount of edges original (input) graph: ', input_graph.number_of_edges()
 
-graph_writer = GraphWriter()
-expander = Expander()
-expanded_graph = expander.expand(input_graph, 3, LineTopology())
-graph_writer.save_edge_list_graph_to_csv(expanded_graph, "facebook_graph_src_new_line_expansion")
-
-"""
-# Ties test for Gephi #
-ties = TIES()
-sampled_graph = ties.sample(input_graph, 0.6)
-
-graph_writer = GraphWriter()
-graph_writer.save_edge_list_graph_to_csv(sampled_graph, "sampled_ca_hepph_graph")
-"""
+#main.run_sampling(facebook_graph_src, "sampled_fb_file", 0.6)
+main.run_expanding(facebook_graph_src, "expanded_fb_file")
